db5e59f45fd95142e3ada97eaa2d6c57ed090360
dbautista9015 <dbautista9015@gmail.com>
2025-07-16 13:17:44 -0700

    Add harpoon

---
+++ lua/custom/plugins/harpoon2.lua
@@ -0,0 +1,70 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        '<leader>a',
+        function()
+          require('harpoon'):list():add()
+        end,
+        desc = 'Harpoon File',
+      },
+      {
+        '<C-E>',
+        function()
+          local harpoon = require 'harpoon'
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'Harpoon Quick Menu',
+      },
+      {
+        '<C-H>',
+        function()
+          require('harpoon'):list():select(1)
+        end,
+        desc = 'Harpoon To File 1',
+      },
+      {
+        '<C-J>',
+        function()
+          require('harpoon'):list():select(2)
+        end,
+        desc = 'Harpoon To File 2',
+      },
+      {
+        '<C-K>',
+        function()
+          require('harpoon'):list():select(3)
+        end,
+        desc = 'Harpoon To File 3',
+      },
+      {
+        '<C-L>',
+        function()
+          require('harpoon'):list():select(4)
+        end,
+        desc = 'Harpoon To File 4',
+      },
+    }
+
+    -- for i = 1, 5 do
+    --   table.insert(keys, {
+    --     '<leader>' .. i,
+    --     function()
+    --       require('harpoon'):list():select(i)
+    --     end,
+    --     desc = 'Harpoon to File ' .. i,
+    --   })
+    -- end
+    return keys
+  end,
+}
